ec12ce89f2ce20d017d18ce643b27a941fb4ec91
Tan Le <letan101104@gmail.com>
2026-02-01 10:15:34 -0500

    Add auditorAwareComponent

--- hub/src/main/java/hazardhub/com/hub/HubApplication.java
+++ hub/src/main/java/hazardhub/com/hub/HubApplication.java
@@ -2,8 +2,10 @@ package hazardhub.com.hub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class HubApplication {
 
 	public static void main(String[] args) {
