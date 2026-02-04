d1eded3ce1896248cf312401c762bd1b9d9b598b
Tan Le <letan101104@gmail.com>
2026-02-03 20:27:38 -0500

    Add integration test for HazardVerification

--- hub/src/main/java/hazardhub/com/hub/constants/HazardHubConstants.java
+++ hub/src/main/java/hazardhub/com/hub/constants/HazardHubConstants.java
@@ -2,7 +2,7 @@ package hazardhub.com.hub.constants;
 
 public final class HazardHubConstants {
 
-    public final class Hazard{
+    public static final class Hazard{
         public static Double DEFAULT_AFFECTED_RADIUS_METERS = 50.0;
     }
 }
