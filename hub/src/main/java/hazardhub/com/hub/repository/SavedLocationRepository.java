96dc141e0fb1ad3645ad40355ba90a450b819c29
Tan Le <letan101104@gmail.com>
2026-02-04 21:55:15 -0500

    Initialize repository and service

---
+++ hub/src/main/java/hazardhub/com/hub/repository/SavedLocationRepository.java
@@ -0,0 +1,15 @@
+package hazardhub.com.hub.repository;
+
+import hazardhub.com.hub.model.entity.SavedLocation;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface SavedLocationRepository extends MongoRepository<SavedLocation, String> {
+
+    List<SavedLocation> findByUserId(String userId);
+
+    boolean existsByUserIdAndName(String userId, String name);
+}
